a63cb0e0f71c686ccdcc3d30574305d1319aaae6
acmcee <acmcee@gmail.com>
2016-01-09 16:54:46 +0800

    modify connect string

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ $con = mysqli_connect( "192.168.56.102","root","root","oracle_list");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
-  } 
+  }
+else 
+{
+    echo "success";
+    
+}    
 ?>
