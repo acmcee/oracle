d0300e48760e660eaa54c32b9edb00450611401a
acmcee <acmcee@gmail.com>
2016-01-09 16:02:20 +0800

    modify connect string

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-$con = mysqli_connect( 
- "192.168.56.102", /* The host to connect to 连接MySQL地址 */   
- "root",   /* The user to connect as 连接MySQL用户名 */   
- "root", /* The password to use 连接MySQL密码 */   
- "oracle_list");  /* The default database to query 连接数据库名称*/   
+$con = mysql_connect( "192.168.56.102","root", "root");
 
-if (mysqli_connect_errno($con))
+if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
