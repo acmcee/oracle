68e6977722feeee4b1a5e2a0698f624e29544182
acmcee <acmcee@gmail.com>
2016-01-10 02:53:40 +0800

    split foot.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+</br>
+</br>
+</br>
+<div id="foot">
+<a href='index.php?page=help'> 使用帮助</a> &nbsp &nbsp <a href='mailto:wanghangwei@zj.chinamobile.com'> 联系我</a>
+<p>©CopyRight 2016-2016 example.com  All Rights Reserved. 测试也有版权，请尊重</p>
+</div>
