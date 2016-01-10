6c769ead9056b5b649f3e421ad2c95742eae10e5
acmcee <acmcee@gmail.com>
2016-01-10 14:01:49 +0800

    fix refresh bug

--- main.php
+++ main.php
@@ -10,8 +10,9 @@
 <body>
 <!-- 输出系统等级-->
 <?php echo "hello";
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
 if(!$isview){
-    ini_set("error_reporting","E_ALL & ~E_NOTICE");
+    
     echo "dir";
 }
 ?>
