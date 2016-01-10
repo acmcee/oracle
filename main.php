144d84d8b87b221fcde8711dbd5da423300bbd8b
acmcee <acmcee@gmail.com>
2016-01-10 14:22:29 +0800

    delete comment

--- main.php
+++ main.php
@@ -12,7 +12,6 @@ ini_set("error_reporting","E_ALL & ~E_NOTICE");
 if(!$isview){
     echo "<div class =\"smile\"> <div class=\"bigsmile\">:) </div>  DON'T OPEN THIS PAGE! <br />
     <div class=\"ssmile\">redirecting...</div></div> <br />";
-    echo "dir";
     header("refresh:2;url=index.php");
     exit;
 }
